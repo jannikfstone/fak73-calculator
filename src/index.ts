3a47ab278c943cd24ab7069ccb951186a1e726c1
Jannik Falkenstein <jannik-private@Janniks-Laptop.local>
2025-02-11 21:02:54 +0100

    add logging

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ const app = new Hono()
 let calcValue = 0
 
 app.get('/', (c) => {
+    console.log("Received request on root")
     return c.text('Hello, World! I am a calculator')
 })
 
 app.get('/add', (c) => {
+    console.log("Received request on add")
   const addValue =  +(c.req.query('y') || 0)
   calcValue += addValue
     return c.json({ value: calcValue })
